9e39d07466d6a4557fd3c15b90f2fc4f7c87d071
Casey Young <caseyyoungnola@gmail.com>
2020-09-12 18:47:31 -0500

    Implement base funtionality for modifying user list.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,28 +2,32 @@ const router = require('express').Router();
 const userData = require('../../data/users.json');
 
 const users = [...userData];
-
+const findUserIndex = (email) => {
+    return users.findIndex(user => user.email === email);
+};
 router.get('/', (req, res) => {
-    res.send(200, users);
+    res.status(200).json(users);
 });
 
 router.post('/', (req, res) => {
-    // TODO: add user to user list
-    res.send(201, req.body);
+    users.push(req.body);
+    res.status(201).json(req.body);
 });
 
 router.get('/:email', (req, res) => {
-    res.send(200, users.find(user => user.email === req.params.email));
+    res.status(200).json(users.find(user => user.email === req.params.email));
 });
 
 router.put('/:email', (req, res) => {
-    // TODO: update specified user in user list
-    res.send(200, req.body);
+    const userIndex = findUserIndex(req.params.email);
+    users.splice(userIndex, 1, req.body);
+    res.status(200).json(users[userIndex]);
 });
 
 router.delete('/:email', (req, res) => {
-    // TODO: delete specified user in user list
-    res.send(200, req.body);
+    const userIndex = findUserIndex(req.params.email);;
+    users.splice(userIndex, 1);
+    res.status(200).send(`User ${req.params.email} successfully deleted.`);
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const UserController = require('./controllers/userController.js');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', UserController);
 
 app.get('/', (req, res) => {
